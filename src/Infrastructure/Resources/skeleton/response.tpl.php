2e16cfcc936efd531f0b682b637b42eb0c8b54e9
TeissierYannis <yannis.teissier70@gmail.com>
2021-01-20 18:04:53 +0100

    init base project

---
+++ src/Infrastructure/Resources/skeleton/response.tpl.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "<?php\n" ?>
+
+namespace <?php echo $namespace; ?>;
+
+/**
+ * Class <?php echo $className; ?><?php echo "\n" ?>
+ * @package <?php echo $namespace; ?><?php echo "\n" ?>
+ */
+class <?php echo $className; ?><?php echo "\n" ?>
+{
+
+}
